54c59a4738d40881c92ea0e2460671ec3d1ba4d7
coding-to-music <connors.tom@gmail.com>
2023-07-05 23:34:19 -0500

    Build

---
+++ visualizations/site-names-most-common-bar-label-rotation2.js
@@ -0,0 +1,143 @@
+const queryData = [
+  { first_dt: "2023-07-01", site_name_txt: "dailymail.co.uk", num_count: 2 },
+  { first_dt: "2023-07-01", site_name_txt: "msn.com", num_count: 16 },
+  { first_dt: "2023-07-01", site_name_txt: "news.yahoo.com", num_count: 6 },
+  { first_dt: "2023-07-01", site_name_txt: "wsj.com", num_count: 6 },
+  { first_dt: "2023-07-02", site_name_txt: "dailymail.co.uk", num_count: 1 },
+  { first_dt: "2023-07-02", site_name_txt: "msn.com", num_count: 3 },
+  { first_dt: "2023-07-02", site_name_txt: "news.yahoo.com", num_count: 2 },
+  { first_dt: "2023-07-02", site_name_txt: "wsj.com", num_count: 4 },
+  { first_dt: "2023-07-03", site_name_txt: "dailymail.co.uk", num_count: 5 },
+  { first_dt: "2023-07-03", site_name_txt: "msn.com", num_count: 9 },
+  { first_dt: "2023-07-03", site_name_txt: "news.yahoo.com", num_count: 2 },
+  { first_dt: "2023-07-03", site_name_txt: "wsj.com", num_count: 9 },
+  { first_dt: "2023-07-04", site_name_txt: "dailymail.co.uk", num_count: 8 },
+  { first_dt: "2023-07-04", site_name_txt: "msn.com", num_count: 6 },
+  { first_dt: "2023-07-04", site_name_txt: "news.yahoo.com", num_count: 2 },
+  { first_dt: "2023-07-04", site_name_txt: "wsj.com", num_count: 7 },
+  { first_dt: "2023-07-05", site_name_txt: "dailymail.co.uk", num_count: 7 },
+  { first_dt: "2023-07-05", site_name_txt: "msn.com", num_count: 17 },
+  { first_dt: "2023-07-05", site_name_txt: "news.yahoo.com", num_count: 7 },
+  { first_dt: "2023-07-05", site_name_txt: "wsj.com", num_count: 9 },
+];
+
+const posList = [
+  "left",
+  "right",
+  "top",
+  "bottom",
+  "inside",
+  "insideTop",
+  "insideLeft",
+  "insideRight",
+  "insideBottom",
+  "insideTopLeft",
+  "insideTopRight",
+  "insideBottomLeft",
+  "insideBottomRight",
+];
+
+echarts.configParameters = {
+  rotate: {
+    min: -90,
+    max: 90,
+  },
+  align: {
+    options: {
+      left: "left",
+      center: "center",
+      right: "right",
+    },
+  },
+  verticalAlign: {
+    options: {
+      top: "top",
+      middle: "middle",
+      bottom: "bottom",
+    },
+  },
+  position: {
+    options: posList.reduce(function (map, pos) {
+      map[pos] = pos;
+      return map;
+    }, {}),
+  },
+  distance: {
+    min: 0,
+    max: 100,
+  },
+};
+
+const labelOption = {
+  show: true,
+  position: echarts.config.position,
+  distance: echarts.config.distance,
+  align: echarts.config.align,
+  verticalAlign: echarts.config.verticalAlign,
+  rotate: echarts.config.rotate,
+  formatter: "{c}  {name|{a}}",
+  fontSize: 16,
+  rich: {
+    name: {},
+  },
+};
+
+// Generate labels dynamically
+labelOption.formatter = function (params) {
+  return `${params.value}  {name|${params.name}}`;
+};
+
+// Generate series data dynamically
+const seriesData = queryData.map((data) => ({
+  name: data.site_name_txt,
+  type: "bar",
+  label: labelOption,
+  emphasis: {
+    focus: "series",
+  },
+  data: [data.num_count],
+}));
+
+// Assign series data to the chart
+return {
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      type: "shadow",
+    },
+  },
+  legend: {
+    data: queryData.map((data) => data.site_name_txt), // Generate legend dynamically
+  },
+  toolbox: {
+    show: true,
+    orient: "vertical",
+    left: "right",
+    top: "center",
+    feature: {
+      mark: { show: true },
+      dataView: { show: true, readOnly: false },
+      magicType: { show: true, type: ["line", "bar", "stack"] },
+      restore: { show: true },
+      saveAsImage: { show: true },
+    },
+  },
+  xAxis: [
+    {
+      type: "category",
+      axisTick: { show: false },
+      data: ["2012", "2013", "2014", "2015", "2016"],
+    },
+  ],
+  yAxis: [
+    {
+      type: "value",
+    },
+  ],
+  series: seriesData, // Assign dynamically generated series data
+  grid: {
+    bottom: "3%",
+    containLabel: true,
+    left: "3%",
+  },
+};
